88ea94f71f05e2944e5f4bbdbf9d48851740e478
Diogo Malagueta <mrmalagueta@gmail.com>
2022-01-19 01:05:25 +0000

    bug solved

--- formExercicio.js
+++ formExercicio.js
@@ -25,12 +25,12 @@ let addButton = document.querySelector('#addButton');
 let description = document.getElementById('itemDescription');
 
 addButton.addEventListener('click', addEvent, false)
-let ul = document.querySelector('ul');
+let lista = document.querySelector('ul');
 function addEvent(e){
     e.preventDefault();
     let li = document.createElement('li');
     li.textContent = description.value;
-    ul.appendChild(li);
+    lista.appendChild(li);
     form.setAttribute('style', 'display:none');
     newItem.removeAttribute('style', 'display:none');
     description.value = '';
@@ -38,8 +38,21 @@ function addEvent(e){
 };
 
 // 4 - verificar class complete e adicionar caso nao tenha
-let items = document.querySelectorAll('li');
+lista.addEventListener('click', clickEliminar, false);
 
+function clickEliminar(e) {
+    e.preventDefault();
+    let item = e.target;
+    if (item.classList.contains('complete')){
+        lista.removeChild(item);
+        nrItems();
+    } else{
+        item.classList.add('complete');
+    }
+}
+
+
+/* let items = document.querySelectorAll('li');
 for (const item of items) {
     item.addEventListener('click', () => {
         if (item.classList.contains('complete')){
@@ -49,7 +62,7 @@ for (const item of items) {
             item.classList.add('complete');
         }
     }, false);
-}
+} */
 
 function removeItem() {}
 
@@ -58,7 +71,7 @@ let span = document.createElement('span');
 let h2 = document.querySelector('h2');
 
 function nrItems(){
-    span.textContent=ul.childElementCount;
+    span.textContent=lista.childElementCount;
     h2.appendChild(span);
 }
 
