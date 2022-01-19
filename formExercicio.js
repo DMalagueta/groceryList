6408780f23e667ae84e5b7770ee151197420117d
Diogo Malagueta <mrmalagueta@gmail.com>
2022-01-19 22:01:45 +0000

    update version

--- formExercicio.js
+++ formExercicio.js
@@ -7,74 +7,79 @@
 */
 
 
-// 1 - esconder form
-let form = document.querySelector('form');
-form.setAttribute('style', 'display:none');
+////// Declarar Variaveis globais
+let newItemButton   = document.querySelector('#newItemButton');
+let newItemForm     = document.querySelector('#newItemForm');
+let showForm        = document.querySelector('#showForm');
+let itemDescription = document.querySelector('#itemDescription');
+let addButton       = document.querySelector('#addButton');
+let ul              = document.querySelector('ul');
+let h2              = document.querySelector('h2')
 
-// 2 - Click no buttao newitem
-let showForm = document.getElementById('showForm');
-let newItem = document.getElementById('newItemButton');
+// 1 - esconder o form, e deixar visivel somente o botao NewItem
+newItemButton.className = 'show';
+newItemForm.className = 'hide';
 
-showForm.addEventListener('click', () => {
-    newItem.setAttribute('style', 'display:none');
-    form.removeAttribute('style','display:none');
-}, false);
+// atualizar contador 
+refreshCounter();
 
-// 3 - Add
-let addButton = document.querySelector('#addButton');
-let description = document.getElementById('itemDescription');
+// 2 - Ao clicar no botao NewItem mostrar o form e esconder o botao
+showForm.addEventListener('click', displayForm, false);
 
-addButton.addEventListener('click', addEvent, false)
-let lista = document.querySelector('ul');
-function addEvent(e){
+function displayForm(){
+    newItemButton.className = 'hide';
+    newItemForm.className = 'show';
+
+    itemDescription.focus();
+}
+
+// 3 - escrevendo um novo produto e clicando no boto "Add", acrescentar novo item á lista, voltar a esconder o form e mostrar botao NovoItem
+
+newItemForm.addEventListener('submit',onSubmit,false);
+
+function onSubmit(e){
+    let newItem = document.createElement('li');
+    newItem.textContent = itemDescription.value;
+
+    ul.insertBefore(newItem, ul.firstElementChild);
+    newItemButton.className = 'show';
+    newItemForm.className = 'hide';
+
+    itemDescription.value = '';
     e.preventDefault();
-    let li = document.createElement('li');
-    li.textContent = description.value;
-    lista.appendChild(li);
-    form.setAttribute('style', 'display:none');
-    newItem.removeAttribute('style', 'display:none');
-    description.value = '';
-    nrItems();
-};
+    refreshCounter();
+}
 
-// 4 - verificar class complete e adicionar caso nao tenha
-lista.addEventListener('click', clickEliminar, false);
+// 4 - ao clicar num item, verificar se tem a class 'complete', se tiver eliminar o item, senao aplicar a class complete, e mover para o final da lista
 
-function clickEliminar(e) {
-    e.preventDefault();
-    let item = e.target;
-    if (item.classList.contains('complete')){
-        lista.removeChild(item);
-        nrItems();
-    } else{
-        item.classList.add('complete');
-        lista.appendChild(item);
+ul.addEventListener('click', deleteCompleteItem, false);
+function deleteCompleteItem(e){
+    let itemClicked = e.target;
+
+    /* if(e.target.localName === 'em'){
+        itemclicked = e.target;
+    } */
+
+    if (itemClicked.className === 'complete'){
+        itemClicked.remove();
+    }else{
+        itemClicked.className = 'complete';
+        ul.appendChild(itemClicked);
     }
+    refreshCounter();
+}
+
+// 5 - apresentar no H2, inserindo dentro de uma tag <span> o numero de items por comprar
+
+function refreshCounter(){
+    /* let items = ul.children.length;
+    let itemsComplete = ul.querySelectorAll('li.complete').length; */
+
+    let itemsNotComplete = ul.querySelectorAll('li:not(.complete)').length;
+
+    h2.innerHTML = `<h2>Buy Groceries <span>${itemsNotComplete}</span></h2>`;
 }
 
 
-/* let items = document.querySelectorAll('li');
-for (const item of items) {
-    item.addEventListener('click', () => {
-        if (item.classList.contains('complete')){
-            item.remove();
-            nrItems();
-        } else{
-            item.classList.add('complete');
-        }
-    }, false);
-} */
 
-function removeItem() {}
-
-// 5 - apresentar o numero de items no h2 
-let span = document.createElement('span');
-let h2 = document.querySelector('h2');
-
-function nrItems(){
-    span.textContent=lista.childElementCount;
-    h2.appendChild(span);
-}
-
-nrItems();
 
